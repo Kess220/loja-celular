c45480af72846d32e0b383da85344d5136be1505
Kess220 <prodvictorkaio@gmail.com>
2022-11-22 20:33:02 -0300

    error

---
+++ loja-celular/src/Content.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Switch, Route } from "react-router-dom";
+import { Store } from "./pages/Store";
+
+export const Content = () => {
+  return (
+    <Switch>
+      <Route path="/" component={Store} />
+    </Switch>
+  );
+};
