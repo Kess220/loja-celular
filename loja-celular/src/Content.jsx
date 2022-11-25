f3f31960c15da12c4b2d47d7b01f04587d0529da
Kess220 <prodvictorkaio@gmail.com>
2022-11-25 19:06:46 -0300

    faltando css

--- loja-celular/src/Content.jsx
+++ loja-celular/src/Content.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
 import { Store } from "./pages/Store";
-
+import { Cart } from "./pages/Cart";
 export const Content = () => {
   return (
     <Switch>
+      <Route path="/cart" component={Cart} />
       <Route path="/" component={Store} />
     </Switch>
   );
--- loja-celular/src/pages/Store.jsx
+++ loja-celular/src/pages/Store.jsx
@@ -1,26 +1,49 @@
 import React, { useEffect, useState } from "react";
-
+import { BsFillCartPlusFill, BsFillCartCheckFill } from "react-icons/bs";
+import { getItem, setItem } from "../services/LocalStorageFuncs";
+import { Link } from "react-router-dom";
 export const Store = () => {
   const [data, setData] = useState([]);
+  const [cart, setCart] = useState(getItem("carrinho") || []);
   useEffect(() => {
     const fetchApi = async () => {
       const url = "https://api.mercadolibre.com/sites/MLB/search?q=celular";
-      const response = await fetch.json(url);
-      const objJson = await response.json;
+      const response = await fetch(url);
+      const objJson = await response.json();
       setData(objJson.results);
     };
     fetchApi();
   }, []);
 
+  const handleClick = (obj) => {
+    const element = cart.find((e) => e.id === obj.id);
+    if (element) {
+      const arrFilter = cart.filter((e) => e.id !== obj.id);
+      setCart(arrFilter);
+      setItem("carrinho", arrFilter);
+    } else {
+      setCart([...cart, obj]);
+      setItem("carrinho", [...cart, obj]);
+    }
+  };
+
   return (
     <div>
       <h1>Store</h1>
       <div>
+        <Link to="/cart">Carrinho</Link>
         {data.map((e) => (
           <div key={e.id}>
             <h4>{e.title}</h4>
             <img src={e.thumbnail} alt="" />
             <h4>{e.price}</h4>
+            <button onClick={() => handleClick(e)}>
+              {cart.some((itemCart) => itemCart.id === e.id) ? (
+                <BsFillCartCheckFill />
+              ) : (
+                <BsFillCartPlusFill />
+              )}
+            </button>
           </div>
         ))}
       </div>
